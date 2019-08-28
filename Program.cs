95b8f9bdb7ac72319c0aa453d625beebae7e787f
yabetsu93 <jabpau93@gmail.com>
2019-08-28 16:56:22 +0900

    virtual machine

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace virtual_machine
 {
@@ -6,7 +8,33 @@ namespace virtual_machine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("input the total memory");
+            var input = Console.ReadLine();
+            var foregroundApps = new List<Dictionary<int, int>>();
+            var backgroundApps = new List<Dictionary<int, int>>();
+            var outputApps = new List<Dictionary<int, int>>();
+
+            // foreground apps
+            foregroundApps.Add(new Dictionary<int, int>{
+                {1,10},
+                {2,15},
+                {3,17}
+            });
+
+            // background apps
+            backgroundApps.Add(new Dictionary<int, int>{
+                {1,3},
+                {2,2},
+            });
+
+            for(int i = 0; i < foregroundApps.Count; i++) {
+                for(int x = 0; x < backgroundApps.Count; x++) {
+                    var foregound = foregroundApps[i];
+                    var background = backgroundApps[x];
+
+            
+                }
+            }
         }
     }
 }
